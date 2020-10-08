5ffeefc73b99bc95a1430e9df110efe5da5e5d9c
Fernando de Paula <dipaula018@gmail.com>
2020-10-08 11:11:41 -0300

    feat: tabs

---
+++ TabExample/app/src/main/java/br/com/fernando/tabexample/Fragment/HomeFragment.java
@@ -0,0 +1,27 @@
+package br.com.fernando.tabexample.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import br.com.fernando.tabexample.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ TabExample/app/src/main/java/br/com/fernando/tabexample/Fragment/SubscriptionFragment.java
@@ -0,0 +1,27 @@
+package br.com.fernando.tabexample.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import br.com.fernando.tabexample.R;
+
+
+public class SubscriptionFragment extends Fragment {
+
+
+    public SubscriptionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subscription, container, false);
+    }
+}
